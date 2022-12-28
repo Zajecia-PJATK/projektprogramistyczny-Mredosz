29027cc28a79cbf3508b71f8cbf652d4f071b98b
Mredosz <mredosz4@gmail.com>
2022-12-28 14:52:54 +0100

    enter file name and word hellow to players

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -14,16 +14,25 @@ struct zero_point{
 };
 
 int** create_matrix(int n,int m);
+void move();
+
+
 
 
 int main(){
+    //Enter file name and say hello to players
+    string file_name;
+    cout<<"                                             HELLO TO MY 2048 GAME" <<endl;
+    cin>>file_name;
+
+    //Enter length and width and create matrix
 
     zero_point *dimension = new zero_point;
     cout<<"Enter length of the board: "<<endl;
     cin>>dimension->length;
     cout<<"Enter width of the board: "<<endl;
     cin>>dimension->width;
-    create_matrix( dimension->length, dimension->width);
+   int** arr= create_matrix( dimension->length, dimension->width);
 
 
     return 0;
